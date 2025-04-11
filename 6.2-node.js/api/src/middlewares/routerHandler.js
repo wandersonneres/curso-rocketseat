e1372cca0fa0e2c25936f3931b4a5dff29bad3c8
wandersonneres <36671464+wandersonneres@users.noreply.github.com>
2025-04-11 16:41:27 -0300

    finalizando primeira api

--- 6.2-node.js/api/src/middlewares/routerHandler.js
+++ 6.2-node.js/api/src/middlewares/routerHandler.js
@@ -1,5 +1,8 @@
 import { routes } from "../routes.js"
 import { extractQueryParams } from "../utils/extract-query-params.js"
+import { Database } from "../database.js"
+
+const database = new Database()
 
 export function routerHandler(request, response) {
     const route = routes.find((route) => {
@@ -15,7 +18,7 @@ export function routerHandler(request, response) {
 
         request.query = query ? extractQueryParams(query) : {}
         
-        return route.controller(request, response)
+        return route.controller({request, response, database})
     } 
 
     return response.writeHead(404).end("Rota não encontrada!")
