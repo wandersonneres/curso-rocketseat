4b83cd4e7028797cea0b44f742a4e27b63943ba3
wandersonneres <36671464+wandersonneres@users.noreply.github.com>
2025-04-11 17:15:56 -0300

    iniciando api rest

---
+++ 6.3-api-rest/src/server.js
@@ -0,0 +1,5 @@
+function sum(a, b) {
+    return a + b;
+}
+var result = sum(5, 3);
+console.log("Resultado da soma", result);
---
+++ 6.3-api-rest/src/server.ts
@@ -0,0 +1,7 @@
+function sum(a: number, b: number): number {
+    return a + b
+}
+
+const result: number = sum(5,3)
+
+console.log("Resultado da soma", result)
